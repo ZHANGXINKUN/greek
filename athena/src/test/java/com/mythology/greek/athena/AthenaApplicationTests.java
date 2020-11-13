8be6fc5f00d47ad5cddb16f9cceef190ec56525c
zhangxinkun <934936142@qq.com>
2020-11-13 16:06:05 +0800

    reactor

--- athena/src/test/java/com/mythology/greek/athena/AthenaApplicationTests.java
+++ athena/src/test/java/com/mythology/greek/athena/AthenaApplicationTests.java
@@ -8,12 +8,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 
+import javax.annotation.Resource;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AthenaApplicationTests {
 
 
-    @Autowired
+    @Resource
     WebTestClient webTestClient;
 
     @Test
