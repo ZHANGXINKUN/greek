ed0e8e319d0e8e07f534dfa095eb33c27a3b7279
zhangxinkun <934936142@qq.com>
2020-11-13 16:05:43 +0800

    reactor

--- athena/src/main/java/com/mythology/greek/athena/demo/Good.java
+++ athena/src/main/java/com/mythology/greek/athena/demo/Good.java
@@ -1,25 +1,32 @@
 package com.mythology.greek.athena.demo;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 /**
  * @Author : xinkun.zhang
  * @Date : 2020/11/13 10:45
  * @Description :
  */
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Good {
 
     private int id;
     private String name;
     private String price;
 
-    public Good(int id, String name, String price) {
-        this.id = id;
-        this.name = name;
-        this.price = price;
-    }
-
-    public Good() {
-    }
+//    public Good(int id, String name, String price) {
+//        this.id = id;
+//        this.name = name;
+//        this.price = price;
+//    }
+//
+//    public Good() {
+//    }
 
     public int getId() {
         return id;
--- athena/src/test/java/com/mythology/greek/athena/AthenaApplicationTests.java
+++ athena/src/test/java/com/mythology/greek/athena/AthenaApplicationTests.java
@@ -1,13 +1,36 @@
 package com.mythology.greek.athena;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class AthenaApplicationTests {
+
+
+    @Autowired
+    WebTestClient webTestClient;
 
     @Test
     void contextLoads() {
     }
 
+
+    @Test
+    public void helloTest(){
+        webTestClient.get()
+                .uri("/good")
+                .accept(MediaType.TEXT_PLAIN)
+                .exchange()
+                .expectStatus()
+                .isOk()
+                .returnResult(String.class)
+                .getResponseBody()
+                .blockFirst();
+    }
 }
